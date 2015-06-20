d6635b9bd814a4cbb1a69ea8856896a5c7196a2c
googolhkl <googolhkl@gmail.com>
2015-06-20 23:27:48 +0900

    arm commit

--- arm/backup/main.cpp
+++ arm/backup/main.cpp
@@ -7,6 +7,20 @@ int main(int argc, char *argv[])
 {
 	int i;
 	
+	/*
+	for( i=0;i<argc;i++)
+	{
+		cout<<argc<<endl;
+		cout<<argv[i]<<endl;
+	}
+
+	char * array[i];
+	for( i=0;i<argc;i++)
+	{
+		array[i]=argv[i];
+	}
+	arm(argc,array);
+	*/
 	char * array[9];
 	cout<<"Light=";
 	char str[10];
